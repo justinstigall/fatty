8692a7266c6e6e3f0b78d61a37974dc602fdc89a
Justin Stigall <justin.stigall@outlook.com>
2025-07-15 14:12:58 -0500

    removing vite stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { FoodDiaryList } from './components/FoodDiaryList'
 import type { FoodDiaryEntry } from './types'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const [diaryEntries] = useState<FoodDiaryEntry[]>([
     {
       id: '1',
@@ -26,23 +22,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <FoodDiaryList entries={diaryEntries} />
     </>
   )
